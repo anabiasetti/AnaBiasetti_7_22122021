3a125dd6a6fe5a8ad728752326cdf228803b567e
Ana Biasetti <anabiasetti@protonmail.com>
2021-12-27 23:30:33 +0100

    Creation of body components

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,93 @@
+import "./sidebar.css";
+import { RssFeed, Comment, Group, Bookmarks, HelpOutline, Event, Today, PlayCircleOutline } from "@material-ui/icons";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarlistItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Comment className="sidebarIcon" />
+            <span className="sidebarlistItemText">Chats</span>
+          </li>
+          <li className="sidebarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarlistItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmarks className="sidebarIcon" />
+            <span className="sidebarlistItemText">Bookmarks</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutline className="sidebarIcon" />
+            <span className="sidebarlistItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarlistItemText">Events</span>
+          </li>
+          <li className="sidebarListItem">
+            <Today className="sidebarIcon" />
+            <span className="sidebarlistItemText">Today</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircleOutline className="sidebarIcon" />
+            <span className="sidebarlistItemText">Videos</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarColleagueList">
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+          <li className="sidebarColleague">
+            <img className="sidebarColleaguePicture" src="/assets/person/2.jpeg" alt="photocolleague" />
+            <span className="sidebarColleagueName">Jean Castex</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
