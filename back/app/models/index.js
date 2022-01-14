ac310a7ed4338cab5e39bc31ea2c37c82f28c8dc
Ana Biasetti <anabiasetti@protonmail.com>
2022-01-14 15:18:53 +0100

    connect one to many users and comments

--- back/app/models/index.js
+++ back/app/models/index.js
@@ -22,24 +22,31 @@ db.sequelize = sequelize;
 db.post = require("../models/post.model.js")(sequelize, Sequelize);
 db.postlike = require("../models/postlike.model.js")(sequelize, Sequelize);
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.comment = require("../models/comment.model.js")(sequelize, Sequelize);
 
-// one to many
+// one to many users & posts(one user can have many posts but one post can have just one user)
 db.user.hasMany(db.post, { as: "posts" });
 db.post.belongsTo(db.user, {
   foreignKey: "userId",
   as: "user",
 });
-// one to many
+// one to many postliked & users(one user can have many post likeds but the post liked has just one user)
 db.user.hasMany(db.postlike, { as: "postlikes" });
 db.postlike.belongsTo(db.user, {
   foreignKey: "userId",
   as: "user",
 });
-// one to many
+// one to many (one post can have many likes but one like or dislike can have just to one post)
 db.post.hasMany(db.postlike, { as: "postlikes" });
 db.postlike.belongsTo(db.post, {
   foreignKey: "postId",
   as: "post",
+});
+// one to many users & comments(one user can make many comments but one comment belongs to one user)
+db.user.hasMany(db.comment, { as: "comments" });
+db.comment.belongsTo(db.user, {
+  foreignKey: "userId",
+  as: "user",
 });
 
 module.exports = db;
